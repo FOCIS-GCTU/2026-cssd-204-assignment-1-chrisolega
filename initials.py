551f341d621840277a3949e6ddec2fb20f793dee
Christian2425402198 <2425402198@live.gctu.edu.gh>
2026-05-30 22:23:45 +0000

    Completed initials assignment in block letters

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
-
+# File: initials.py
+# Description: Prints my initials (CGT) in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Christian Gift Tetteh
+# STUDENT ID: 2425402198
+# Email: 2425402198@live.gctu.edu.gh
+# Grader:Agustus
+#
+# On my honor, Christian Gift Tetteh, this programming assignment is my own work
+# and I have not provided this code to any other student.
 def main():
-  pass #code goes here
+    print()
 
+    print("...CGT")
+
+    print()
+
+    #The print out of the final initials
+    print("...CCCCCCCCCCCC......GGGGGGGGGGGG......TTTTTTTTTTTT....")
+    print("...CCCCCCCCCCCC......GGGGGGGGGGGG......TTTTTTTTTTTT....")
+    print("...CCC...............GGG...................TTTT........")
+    print("...CCC...............GGG...................TTTT........")
+    print("...CCC...............GGG...GGGGGG..........TTTT........")
+    print("...CCC...............GGG...GGGGGG..........TTTT........")
+    print("...CCC...............GGG......GGG..........TTTT........")
+    print("...CCC...............GGG......GGG..........TTTT........")
+    print("...CCCCCCCCCCCC...**.GGGGGGGGGGGG.**.......TTTT.....**.")
+    print("...CCCCCCCCCCCC...**..GGGGGGGGGG..**.......TTTT.....**.")
+
+    print()
 
 
 main()
+
